f4c4850c3ffa891c3f4d8d70cd8856e30fb0ac45
Erick Coutinho <erickcoutinho08@hotmail.com>
2023-02-26 21:56:06 -0300

    Atualização de texto

--- eita.js
+++ eita.js
@@ -1,7 +1,37 @@
-var vel = 60.1 
-if (vel > 60) {
-    console.log('Você foi multado!')
+//exercício 1
+    //window.alert('eae parça')
+    //window.confirm('salve cria')
+    //window.prompt('eita mds')
+
+    //exercício 2
+    //var n1 = Number.parseFloat(window.prompt ('número 1'))
+    //var n2 = Number.parseFloat(window.prompt ('número 2'))
+    //var s = n1 + n2 
+    //window.alert('a soma dos valores é ' + s)
+
+    //exercício 3 - formatando strings
+    //var nome = window.prompt('Seu nome')
+    //document.write(`Seu nome é ${nome}`)
+    //document.write(`Seu nome tem ${nome.length} letras`)
+    //document.write(`Seu nome em maíuscula é ${nome.toUpperCase()}`)
+
+//exercício 4 - elementos DOM funções
+function clicar(){
+    var a = window.document.getElementById('blocot')
+     a.innerText = 'clicou!'
 }
-else{
-    console.log('Não foi multado!')
+
+function clicar2(){
+    var a = window.document.getElementById('area')
+     a.innerText = 'clicou!'
 }
+
+
+
+// var vel = 60.1 
+// if (vel > 60) {
+//     console.log('Você foi multado!')
+// }
+// else{
+//     console.log('Não foi multado!')
+// }
