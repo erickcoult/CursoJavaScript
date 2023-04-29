641e29012ce1d3d95ff1f348569929474537c18d
Erick Coutinho <erickcoutinho08@hotmail.com>
2023-04-28 21:45:19 -0300

    Finalização do javascript

--- Projeto1/exerjs.js
+++ Projeto1/exerjs.js
@@ -1,7 +1,18 @@
 function carregar() {
 var msg = document.getElementById('msg')
-var img = document.getElementById('img1')
+var img = document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas`    
+var minutos = data.getMinutes()
+msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos`
+if(hora >=0 && hora <12){
+    img.src = 'imgs/manha.png'
+    document.body.style.background = '#fba279'
+} else if (hora >= 12 && hora < 18){
+    img.src = 'imgs/tarde.png'
+    document.body.style.background ='#d5a76c'
+} else {
+    img.src = 'imgs/noite.png'
+    document.body.style.background ='#3e2e3e'
+}
 }
